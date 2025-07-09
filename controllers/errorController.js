0560b054f8ddd55abead3537b9943576835a8528
Caleb Muturi <calebmuturi2024@gmail.com>
2025-07-09 18:44:28 +0300

    Handling Moongose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,23 @@
 const AppError = require('./../utils/appError');
 
-const handleCastErrorDB = err => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
-}
+};
 
-const handleDuplicateFieldsDB = err => {
+const handleDuplicateFieldsDB = (err) => {
   // Use err.message instead of err.errmsg
   const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
-  const message = `Duplicate field value: ${value ? value[0] : ''}. Please use another value!`;
+  const message = `Duplicate field value: ${
+    value ? value[0] : ''
+  }. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
@@ -21,7 +30,6 @@ const sendErrorDev = (err, res) => {
   });
 };
 
-
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
@@ -30,7 +38,7 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
 
-  // Programming or other unknown error: don't leak error details
+    // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
     console.error('ERROR 💥', err);
@@ -59,6 +67,8 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
